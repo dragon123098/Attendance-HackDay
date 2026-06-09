77f9c75889dc7537513fdf4f7721856832b8e9b9
Peter Grunig <tanna435@byu.edu>
2026-06-09 11:45:10 -0600

    Added Gerald and UnAuth Base

--- cmd/handlers.go
+++ cmd/handlers.go
@@ -6,8 +6,22 @@ import (
 	"path/filepath"
 )
 
+//idk if this is the right place to this, but I'm putting it here
+//Any pagedata we ned to add to the page, we populate this struct with data from the database
+//This is basically a non persistable entity
+
+type PageData struct {
+	Title       string
+	Username    string
+	AvatarImage string
+}
+
 func loginView(w http.ResponseWriter, r *http.Request) {
-	render(w, "home.html", nil)
+	data := PageData{
+		Title: "Login",
+	}
+
+	renderUnAuth(w, "login.html", data)
 }
 
 func loginSubmitView(w http.ResponseWriter, r *http.Request) {
@@ -20,6 +34,11 @@ func studentView(w http.ResponseWriter, r *http.Request) {
 }
 
 func shopView(w http.ResponseWriter, r *http.Request) {
+	data := PageData{
+		AvatarImage: "/static/images/geraldIcon2.png",
+	}
+
+	render(w, "home.html", data)
 }
 
 func avatarView(w http.ResponseWriter, r *http.Request) {
@@ -40,6 +59,8 @@ func adminView(w http.ResponseWriter, r *http.Request) {
 func adminEditView(w http.ResponseWriter, r *http.Request) {
 }
 
+
+//idk if this is the right place for this, but it makes sense to me
 // render is a helper function to render templates with a base layout
 func render(w http.ResponseWriter, page string, data any) {
 	tmpl, err := loadTemplates(page)
@@ -58,10 +79,32 @@ func render(w http.ResponseWriter, page string, data any) {
 //This will load the templates needed. Just pass in what page you want and it will render it will all the correct stuff
 func loadTemplates(page string) (*template.Template, error) {
 	return template.ParseFiles(
-		filepath.Join("templates", "base.html"),
+		filepath.Join("templates", "AuthBase.html"),
 		filepath.Join("templates", "partials", "topbar.html"),
 		filepath.Join("templates", "partials", "navbar.html"),
 		filepath.Join("templates", "partials", "footer.html"),
 		filepath.Join("templates", page),
 	)
+}
+
+
+//These two functions load pages for unauthenticated users.
+func loadUnAuthTemplates(page string) (*template.Template, error) {
+	return template.ParseFiles(
+		filepath.Join("templates", "UnAuthBase.html"),
+		filepath.Join("templates", page),
+	)
+}
+
+func renderUnAuth(w http.ResponseWriter, page string, data any) {
+	tmpl, err := loadUnAuthTemplates(page)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	err = tmpl.ExecuteTemplate(w, "base", data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
