9dc1ae9bdb152e0a6bba724975109292c2d218ff
Seth Conner <sconner1@byu.edu>
2026-06-09 10:21:07 -0600

    made handlers for all routes in main.go

---
+++ cmd/handlers.go
@@ -0,0 +1,36 @@
+package main
+
+import "net/http"
+
+func loginView(w http.ResponseWriter, r *http.Request) {
+}
+
+func loginSubmitView(w http.ResponseWriter, r *http.Request) {
+}
+
+func logoutView(w http.ResponseWriter, r *http.Request) {
+}
+
+func studentView(w http.ResponseWriter, r *http.Request) {
+}
+
+func shopView(w http.ResponseWriter, r *http.Request) {
+}
+
+func avatarView(w http.ResponseWriter, r *http.Request) {
+}
+
+func coinView(w http.ResponseWriter, r *http.Request) {
+}
+
+func teacherView(w http.ResponseWriter, r *http.Request) {
+}
+
+func teacherEditView(w http.ResponseWriter, r *http.Request) {
+}
+
+func adminView(w http.ResponseWriter, r *http.Request) {
+}
+
+func adminEditView(w http.ResponseWriter, r *http.Request) {
+}
--- cmd/main.go
+++ cmd/main.go
@@ -16,17 +16,19 @@ func main() {
 	//all roles
 	mux.HandleFunc("GET /login", loginView)              	// Returns full page, login view (initial page)
 	mux.HandleFunc("POST /login", loginSubmitView)       	// Returns HTML fragment (HTMX), login submission view
+	mux.HandleFunc("POST /logout", logoutView)       		// Returns HTML fragment (HTMX), logout view
+
 	//student role
-	mux.Handle("/studentDashboard", studentView)			// Returns full page, student dashboard view
+	mux.Handle("GET /studentDashboard", requireRole("student", http.HandlerFunc(studentView)))			// Returns full page, student dashboard view
 	mux.HandleFunc("GET /shop", shopView)        		 	// Returns full page, shop view
 	mux.HandleFunc("GET /avatar", avatarView)        	 	// Returns full page, avatar view
 	mux.HandleFunc("POST /studentDashboard/coin", coinView) // Returns HTML fragment (HTMX), coin popup after marking attendance
 	//teacher role
-	mux.Handle("/teacherDashboard", teacherView)			// Returns full page, teacher dashboard view
-	mux.HandleFunc("POST /teacherDashboard/edit", editView) // Returns HTML fragment (HTMX), edit view
+	mux.Handle("GET /teacherDashboard", requireRole("teacher", http.HandlerFunc(teacherView)))			// Returns full page, teacher dashboard view
+	mux.HandleFunc("POST /teacherDashboard/edit", teacherEditView) // Returns HTML fragment (HTMX), edit view
 	//admin role
-	mux.Handle("/adminDashboard", adminView)				// Returns full page, admin dashboard view
-	mux.HandleFunc("POST /adminDashboard/edit", editView)   // Returns HTML fragment (HTMX), edit view
+	mux.Handle("GET /adminDashboard", requireRole("admin", http.HandlerFunc(adminView)))				// Returns full page, admin dashboard view
+	mux.HandleFunc("POST /adminDashboard/edit", adminEditView)   // Returns HTML fragment (HTMX), edit view
 
 	log.Print("starting server on http://localhost:4000")
 
